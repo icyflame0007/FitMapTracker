4f56906a1cd9cb81fb35db9d5a8f1e022e3382fc
Santosh <nagasantosh.akula@gmai.com>
2024-01-29 19:35:31 +0530

    Added required dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,16 +2,23 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        jcenter()
         gradlePluginPortal()
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+
         google()
+        jcenter()
         mavenCentral()
+        maven(url = "https://jitpack.io")
+
     }
 }
 
 rootProject.name = "FitMap Tracker"
 include(":app")
+
+
